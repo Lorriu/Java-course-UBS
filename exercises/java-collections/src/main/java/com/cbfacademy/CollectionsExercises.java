dfc04032858c34f1caa00a9180dc688a3d7f6781
Lorraine <l.ugen@hotmail.co.uk>
2024-08-13 20:44:35 -0400

    ArrayDeque and HashMaps exercise completed

--- exercises/java-collections/src/main/java/com/cbfacademy/CollectionsExercises.java
+++ exercises/java-collections/src/main/java/com/cbfacademy/CollectionsExercises.java
@@ -13,16 +13,15 @@ public class CollectionsExercises {
         integers.add(5);
         integers.add(6);
         integers.add(8);
-        integers.add(9);
         integers.add(2);
         integers.add(9);
         
         //  - add another 2 as the last element of the list
         integers.addLast(2);
         //  - add 4 as the 3rd element of the list
-        integers.add(3, 4);
+        integers.add(2, 4);
         //  - invoke the method element() on the list and print the result on the screen
-        System.out.println(integers.element());
+        System.out.print(integers.element());
         //  - return the list
         return integers;
         
@@ -37,11 +36,11 @@ public class CollectionsExercises {
         stack.add(8);
         stack.add(9);
         //  - print the first element of the stack on the screen
-        System.out.println(stack.firstElement());
+        System.out.print(stack.firstElement());
         //  - print the last element of the stack on the screen
-        System.out.println(stack.lastElement());
+        System.out.print(stack.lastElement());
         //  - invoke the method pop() on the stack and print the result on the screen
-        System.out.println(stack.pop());
+        System.out.print(stack.pop());
         //  - invoke the push(4) method on the stack
         stack.push(4);
         //  - return the stack
@@ -50,28 +49,48 @@ public class CollectionsExercises {
 
     public ArrayDeque<Integer> useArrayDeque() {
         // TODO: create an empty arrayDeque
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
         //  - add 5, 6, 8, 9 to the queue
+        arrayDeque.add(5);
+        arrayDeque.add(6);
+        arrayDeque.add(8);
+        arrayDeque.add(9);
         //  - print the first element of the queue on the screen
+        System.out.print(arrayDeque.getFirst());
         //  - print the last element of the queue on the screen
+        System.out.print(arrayDeque.getLast());
         //  - invoke the method poll() on the queue and print the result on the screen
+        System.out.print(arrayDeque.poll());
         //  - invoke the element() method on the queue and print the result on the screen
+        System.out.print(arrayDeque.element());
         //  - return the queue
-        throw new RuntimeException("Not implemented");
+        return arrayDeque;
     }
 
     public HashMap<Integer, String> useHashMap() {
         // TODO: create an empty hash map
+        HashMap<Integer, String> hashMap = new HashMap<>();
         //  - add {1, TypeScript} entry to the map
+        hashMap.put(1, "TypeScript");
         //  - add {2, Kotlin} entry to the map
+        hashMap.put(2, "Kotlin");
         //  - add {3, Python} entry to the map
+        hashMap.put(3, "Python");
         //  - add {4, Java} entry to the map
+        hashMap.put(4, "Java");
         //  - add {5, JavaScript} entry to the map
+        hashMap.put(5, "JavaScript");
         //  - add {6, Rust} entry to the map
+        hashMap.put(6, "Rust");
         //  - determine the set of keys from the map and print it on the screen
+        System.out.print(hashMap.keySet());
         //  - determine the set of values from the map and print it on the screen
+        System.out.print(hashMap.values());
         //  - determine whether the map contains "English" as a language and print the result on the screen
+        System.out.print(hashMap.containsValue("English"));
+
         //  - return the map
-        throw new RuntimeException("Not implemented");
+        return hashMap;
     }
 
     public String getName() {
